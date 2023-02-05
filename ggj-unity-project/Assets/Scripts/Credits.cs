fc8a780b40cd0e9c25877d87509af52387f2df97
jesse sheehan <jessetsheehan@gmail.com>
2023-02-05 14:47:05 -0500

    added credits scene and animation

---
+++ ggj-unity-project/Assets/Scripts/Credits.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private Animator creditsAnimator;
+    [SerializeField] private float fastCreditsSpeed;
+    
+   
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            creditsAnimator.speed = fastCreditsSpeed;
+        }
+        
+        
+    }
+    
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene("TitleScreen 1");        
+    }
+    
+}
