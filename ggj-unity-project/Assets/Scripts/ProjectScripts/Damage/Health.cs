c0a83e07ee239afcf6d4b58f646a863c029e3894
cmr624 <cmr624@nyu.edu>
2023-02-05 16:52:53 -0500

    level state scaling stuff and kaiju scene

--- ggj-unity-project/Assets/Scripts/ProjectScripts/Damage/Health.cs
+++ ggj-unity-project/Assets/Scripts/ProjectScripts/Damage/Health.cs
@@ -1,21 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using GGJ2022;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
 
-    public int StartingHealth;
-    private int _currentHealth;
+    [SerializeField] private bool ShouldDestroyOnDeath = true;
+    [SerializeField] private bool ShouldCountTowardsBodycount = true;
+
+    public bool IsInvincible = false;
     
+    
+    public int StartingHealth = 10;
+    private int _currentHealth;
+
+    public UnityEvent OnDeath;
+
+    public UnityEvent OnDamaged;
+    private bool _isDead = false;
     // Start is called before the first frame update
     void OnEnable()
     {
         _currentHealth = StartingHealth;
     }
 
-    public void GetDamaged(DamagePayload dmg)
+    public void DoDamage(int dmg)
     {
-        
+        if (_isDead || IsInvincible) return;
+        OnDamaged?.Invoke();
+         
+        _currentHealth= Math.Max(0, _currentHealth - dmg);
+        if (_currentHealth <= 0)
+        {
+           OnDeath?.Invoke();
+           _isDead = true;
+           if (ShouldCountTowardsBodycount)
+           {
+               LevelStateManager.Instance.IncrementBodyCount();
+           }
+           if (ShouldDestroyOnDeath)
+           {
+               Destroy(gameObject);
+           }
+           
+        }
     } 
 }
--- ggj-unity-project/Assets/Scripts/RollingAttackController.cs
+++ ggj-unity-project/Assets/Scripts/RollingAttackController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using GGJ2022;
 using GGJ2022.Audio;
 using GGJ2022.EnemyAI;
 using UnityEngine;
@@ -26,7 +27,8 @@ public class RollingAttackController : MonoBehaviour
     // Start is called before the first frame update
     void OnEnable()
     {
-        _rigidbody = GetComponent<Rigidbody>(); 
+        _rigidbody = GetComponent<Rigidbody>();
+        _playerHealth = GetComponent<Health>();
     }
 
     // Update is called once per frame
@@ -37,6 +39,7 @@ public class RollingAttackController : MonoBehaviour
             if (Time.time - _rollStartTime > _maxRollTime)
             {
                 _isRolling = false;
+                _playerHealth.IsInvincible = false;
                 OnRollEnd?.Invoke();
             }
         }
@@ -47,22 +50,31 @@ public class RollingAttackController : MonoBehaviour
         if (!_isRolling) return;
 
         _isRolling = false;
+        _playerHealth.IsInvincible = false;
+
         SFXAudioEventDriver.StaticFireSFXEvent("SnailImpact");
         if (collision.gameObject.CompareTag("Targetable"))
         {
-            collision.gameObject.GetComponent<EnemyState>()?.DoDamage(1);
+            int damage = LevelStateManager.Instance.GetDamageRatio();
+            collision.gameObject.GetComponent<EnemyState>()?.DoDamage(damage);
+            collision.gameObject.GetComponent<Health>()?.DoDamage(damage);
+            
             _rigidbody.AddRelativeForce(0.75f * _rollImpulse * (collision.contacts[0].normal + Vector3.up).normalized, ForceMode.Impulse);
         }
         
         OnRollEnd?.Invoke(); 
     }
 
+    private Health _playerHealth;
+    
     public void TriggerRoll(Vector3 direction, bool wasTargeted = false)
     {
         if (_isRolling) return;
 
         _isRolling = true; 
         _rollStartTime = Time.time;
+
+        _playerHealth.IsInvincible = true; 
 
         var force = wasTargeted ? _targetMultiplier * _rollImpulse : _rollImpulse; 
         
